9b999c09d3c1792f86a804c4d6a66b1ec4a2584a
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-11-26 17:18:29 +0100

    add: password transformation, decryption

--- PWmanager/server/EncryptionHandler.js
+++ PWmanager/server/EncryptionHandler.js
@@ -22,6 +22,11 @@ const decrypt = (encryption) => {
     Buffer.from(secret),
     Buffer.from(encryption.iv, "hex")
   );
+  const decryptedPassword = Buffer.concat([
+    decipher.update(Buffer.from(encryption.password, "hex")),
+    decipher.final(),
+  ]);
+  return decryptedPassword.toString();
 };
 
 module.exports = { encrypt, decrypt };
