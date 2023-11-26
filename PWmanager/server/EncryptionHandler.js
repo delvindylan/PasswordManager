5a9b615a109bfcc730a6eb11482971bcfc339ceb
hereIsLucas <lucas.heroin@lernende.bbw.ch>
2023-11-26 17:17:06 +0100

    add: password transformation, encryption

--- PWmanager/server/EncryptionHandler.js
+++ PWmanager/server/EncryptionHandler.js
@@ -1,9 +1,21 @@
 const crypto = require("crypto");
+const secret = "DontTalkToMyMans";
 
 const encrypt = (password) => {
   const iv = Buffer.from(crypto.randomBytes(16));
   const cipher = crypto.createCipheriv("aes-256-ctr", Buffer.from(secret), iv);
+
+  const encryptedPassword = Buffer.concat([
+    cipher.update(password),
+    cipher.final(),
+  ]);
+
+  return {
+    iv: iv.toString("hex"),
+    password: encryptedPassword.toString("hex"),
+  };
 };
+
 const decrypt = (encryption) => {
   const decipher = crypto.createDecipheriv(
     "aes-256-ctr",
